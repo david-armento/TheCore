96331a1d063cc71d7d79b2debcaade973afe1dbc
davidStark87 <david.armento@gmail.com>
2015-06-19 11:54:28 +0200

    Actualización /index.php

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
+
+//Incluimos nucleo
 include('core/core.php');
+
+//Abrimos base de datos
+open_bd();
+
+//Cargamos layout
 include(_PATH_.'layout/default.php');
+
+//Cerramos base de datos
+close_bd();
 ?>
